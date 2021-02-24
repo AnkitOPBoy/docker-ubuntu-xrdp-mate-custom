edea748fc74e68c2e7a89e8ad514e04af18fd47e
AnkitOPBoy <79449964+AnkitOPBoy@users.noreply.github.com>
2021-02-24 08:16:30 -0800

    Update autostartup.sh

--- autostartup.sh
+++ autostartup.sh
@@ -56,3 +56,18 @@ fi
 
 #This has to be the last command!
 /usr/bin/supervisord -n
+# Put the user in the docker group
+sudo usermod -a -G docker $USER
+newgrp docker
+
+# Nvidia Docker
+sudo apt install curl -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
+
+# Check Docker image
+docker run --gpus all nvidia/cuda:10.0-base nvidia-smi
